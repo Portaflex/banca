355bf93b5725f6897a5f677a094af7d8a582ee99
Diego Sala <dsala@clinicadecot.es>
2022-11-15 10:16:00 +0100

    Update Acceso.php

--- Acceso.php
+++ Acceso.php
@@ -3,5 +3,16 @@ namespace portaflex\banca;
 
 class Acceso extends BaseObject
 {
+    public $estado;
+    public $identidad;
     
+    public function identidad() : string
+    {
+        
+    }
+    
+    public function contrasena() : string
+    {
+        
+    }
 }
